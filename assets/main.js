16985758688e7d92d9ce16866cf88970a42b6064
Kyrylo Panin <omd@Kyrylos-MacBook-Pro.local>
2025-09-15 21:35:23 +0200

    Fixed path

--- assets/main.js
+++ assets/main.js
@@ -4,7 +4,7 @@ console.log("inited")
 document.getElementById('make-negr').addEventListener('click', function() {
     console.log("clicked");
     if (!isNegr) {
-        this.parentElement.querySelector('.section-image').setAttribute('src', './assets/lgbt-daniil-negr.png');
+        this.parentElement.querySelector('.section-image').setAttribute('src', 'assets/lgbt-daniil-negr.png');
         this.innerHTML = `Сделать Даню
                 <img class="inline h-4 ml-1 mr-0.2" src="assets/Netflix_logo.svg" alt="netflix_logo"/>
                 ормальным`
@@ -14,6 +14,6 @@ document.getElementById('make-negr').addEventListener('click', function() {
     this.innerHTML = `Сделать Даню
                 <img class="inline h-4 ml-1 mr-0.2" src="assets/Netflix_logo.svg" alt="netflix_logo"/>
                 егром`
-    this.parentElement.querySelector('.section-image').setAttribute('src', './assets/lgbt-daniil.jpg');
+    this.parentElement.querySelector('.section-image').setAttribute('src', 'assets/lgbt-daniil.jpg');
     isNegr = false;
 });
